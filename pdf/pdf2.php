9815557b8b2099759e335e9b51e8fc93f2bfeabe
lautaro Farias <lautarosistemas@gmai.com>
2023-03-16 19:53:15 -0300

    comienzo segunda etapa desarrollo

--- pdf/pdf2.php
+++ pdf/pdf2.php
@@ -6,7 +6,24 @@
     $cod = (isset($_POST['cod'])) ? $_POST['cod'] : '';
 
 
-    switch ($opcion){
+    $url = 'fake.pdf';
+    $_POST['cod'] = '726287e201a0c53944131c6fd314f3871c5368efaba9668702bb2ccf587ab387';
+                    
+                    $mpdf=new \Mpdf\Mpdf();
+                    $css=file_get_contents("../reporte/style.css");
+                    $plantilla=getPlantilla($_POST['cod']);
+                    $mpdf->writeHTML($css,\Mpdf\HTMLParserMode::HEADER_CSS);
+                    $mpdf->writeHTML($plantilla,\Mpdf\HTMLParserMode::HTML_BODY);
+                    $mpdf->output($url,"I");
+                    json_encode($url, JSON_UNESCAPED_UNICODE);
+
+
+
+
+
+
+    /*
+    switch ($opcion) {
         case 1:
             if (isset($_POST['cod'])) {
                     $url = $_POST['cod'].'.pdf';
@@ -34,4 +51,4 @@
             # code...
             break;
     }
-
+    */
